50b1d846154aabfe06a1061759302c728f54bf17
SumaySethia <sumaysethia@gmail.com>
2026-05-14 01:28:24 -0400

    intentional bug to test pipeline

---
+++ .github/workflows/hello.py
@@ -0,0 +1,9 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+def farewell(name):
+    return f"Goodbye, {name}!"
+
+print(greet("World"))
+print(farewell("World"))
+print(this_variable_does_not_exist)
